4ce13ba2369f83e17ca45a4afeb46985fa12c753
sean-attewell <sean.attewell@gmail.com>
2019-02-25 13:42:13 +0000

    MVP

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
+import { Route } from 'react-router-dom';
+
+
+
 const App = () => (
   <div>
     <Navigation />
+    {/* This is where we're going to declare and specify our router. */}
+    <Route exact path='/' component={Home} />
+    <Route path='/about' component={About} />
+    <Route path='/contact' component={Contact} />
   </div>
 );
 
